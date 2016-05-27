e5977852b283b3a75f6f9fa8b7c9b54663b0c00c
Suhaib Khan <suheb.work@gmail.com>
2016-05-27 22:13:10 +0530

    Changing APP_BASE_URL
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- app/src/main/java/com/haloappstudio/morld/Utils.java
+++ app/src/main/java/com/haloappstudio/morld/Utils.java
@@ -4,6 +4,6 @@ package com.haloappstudio.morld;
  * Created by suheb on 26/5/16.
  */
 public class Utils {
-    public static final String API_BASE_URL = "http:/192.168.0.102:5000/";
+    public static final String API_BASE_URL = "http://morldapp.herokuapp.com/";
     public static final String USER = "user";
 }
